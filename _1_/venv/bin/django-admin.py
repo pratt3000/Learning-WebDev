e8e376dd76f032e922aab71568320a8dd695aeca
pratt3000 <pratt3000@gmail.com>
2020-08-27 11:29:01 +0530

    3. Mutations

---
+++ _1_/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/pratt3000/Documents/projects/learning_graphQL/_1_/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
