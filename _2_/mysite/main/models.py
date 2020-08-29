8ea173328d5511d7405afaa8fb166242aad3df4a
pratt3000 <pratt3000@gmail.com>
2020-08-29 17:37:40 +0530

    part 3 donegit add .!

--- _2_/mysite/main/models.py
+++ _2_/mysite/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+# migration need to be done when you add models
+
+class Tutorial(models.Model):
+    tutorial_title = models.CharField(max_length = 200) #when you have to set a max limit
+    tutorial_content = models.TextField()               #no limit - more like an article body
+    tutorial_published = models.DateTimeField("date published", default=datetime.now())
+
+    def __str__(self):
+        return self.tutorial_title
+
